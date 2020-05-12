e60592acad5d139eeedd83717c8603505db28be3
Claudio Dal Molin <dalmcla1@students.zhaw.ch>
2020-05-12 11:25:10 +0200

    Update Camera VR Script

---
+++ Assets/CameraVR_Disable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class CameraVR_Disable : MonoBehaviour
+{
+
+    void Start()
+    {
+        XRSettings.enabled = false;
+    }
+}
+
---
+++ Assets/CameraVR_Enable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class CameraVR_Enable : MonoBehaviour
+{
+    void Start()
+    {
+        XRSettings.enabled = true;
+    }
+}
